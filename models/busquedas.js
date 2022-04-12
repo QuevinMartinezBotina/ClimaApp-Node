c3c80a221aa14d82f1c54b2a936237a300ae9352
quevinmartinezbotina <quevinmartinezbotina@gmail.com>
2022-04-12 16:09:54 -0500

    74. Crear instancias de Axios

--- models/busquedas.js
+++ models/busquedas.js
@@ -7,14 +7,27 @@ class Busquedas {
     //todo: leer DB si existe
   }
 
+  //?Esto es la info de nuestro API con el cual nmos podemos conectar
+  get paramsMapbox() {
+    return {
+      access_token:
+        "pk.eyJ1IjoibG9rZWRyb3giLCJhIjoiY2wxdjN0Y3YyMHNpbjNjbnRmNHA3OGJjbCJ9.E8i7HBXMe23Dy5OVkk6LPg",
+      limit: 5,
+      language: "es",
+    };
+  }
+
   async ciudad(lugar = "") {
     //?Para el manejo de errores
     try {
       //?peticion http para traer datos de la API
-      //console.log(`Ciudad clase: ${lugar}`);
-      const resp = await axios.get(
-        "https://api.mapbox.com/geocoding/v5/mapbox.places/Madrid.json?access_token=pk.eyJ1IjoibG9rZWRyb3giLCJhIjoiY2wxdjN0Y3YyMHNpbjNjbnRmNHA3OGJjbCJ9.E8i7HBXMe23Dy5OVkk6LPg&limit=5&language=es"
-      );
+      const intance = axios.create({
+        baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`, //?Esto es nuestro endpoint para traer datos
+        params: this.paramsMapbox, //?Esta es nuestra infod e acceso al API
+      });
+
+      const resp = await intance.get();
+
       console.log(resp.data);
 
       return []; //?Retornamos un arreglo
